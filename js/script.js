71e51396cceb499bc20ba8e1b84bb1ef60d03a24
KOVU9529 <bapgiordano@gmail.com>
2022-05-31 16:15:22 +0200

    lovoro-completo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const myName= prompt('Qual è il tuo nome?')
 const mySurname= prompt('Qual è il tuo cognome?')
 const myFavoriteColor= prompt('Qual è il tuo colore preferito?')
+const myNumber=21;
 
-const myAnswers= myName +' '+ mySurname +' '+ myFavoriteColor
+const myAnswers= myName + mySurname + myFavoriteColor + myNumber
 
 document.getElementById ('answers').innerHTML= myAnswers;
